f14353d9dd07fdcff89265bb05dc9d9e3bd19df0
Kat <kemetcalf3.14@gmail.com>
2022-03-29 21:30:18 -0700

    Banking App Complete(unpolished)

--- Banking_App.py
+++ Banking_App.py
@@ -1,55 +1,51 @@
-# TODO: USE:
-# WHILE TRUE
-# INPUT X
-# CLASSES X
-# METHOD X
-# PROPERTIES x
-# OPEN() x
-
 import math
 import datetime
-# TODO: CLASS BASED BANKING APP
 
 
 class BankingApp:
     with open("account_ledger.txt", "w") as account_ledger:
         init_deposit = input("Please make an initial deposit: ")
-        account_bal = float(init_deposit)
         account_start = datetime.datetime.now()
         ledger = account_ledger.write(
-            f"\n {init_deposit} starting balance {account_start.strftime('%c')}")
-    print(account_bal)  # initial account balance established
+            f"starting balance as of {account_start.strftime('%c')} \n {init_deposit} ")
+    print(f"Current Balance: {init_deposit}")
 
-    # TODO: WITHDRAWAL
+    def write_changes(self, change, new_balance):
+        # writes newly calculated balance to ledger file
+        ledger = open("account_ledger.txt", "a")
+        ledger.write(change)
+        new_balance_str = "\n {0:.2f} \n".format(
+            new_balance)
+        print("\nCurrent Balance: ", new_balance_str)
+        ledger.write(new_balance_str)
+        ledger.close()
 
     def select(self):
         pick = input(
-            "Would you like to \n(W): Withdraw from your account \n(D): Deposit to your account or \n(V): View your account?")
+            "Would you like to \n(W): Withdraw from your account \n(D): Deposit to your account \n(V): View your account or \n(Q): Quit?")
         return pick
 
     def withdrawal(self):
         deduct = input("How much would you like to withdraw?")
-        new_balance = self.account_bal - float(deduct)
-        print(new_balance)
-        # change = str(0 - float(deduct))
-        # TODO: WRITE METHOD RESULT TO A PYTHON LEDGER FILE
-        ledger = open("account_ledger.txt", "a")
-        ledger.write(f"\n-{deduct}")
-        new_balance_f = new_balance.format('%c')
-        ledger.write(f"\n {str(new_balance_f)} balance")
-        ledger.close()
 
-    # TODO: DEPOSIT METHODS
+        with open("account_ledger.txt", "r") as f:
+            current_bal = f.readlines()[-1]
+
+        change = f"\n-{deduct}"
+        new_balance = float(current_bal) - float(deduct)
+
+        self.write_changes(change, new_balance)
+
     def deposit(self):
         add = input("How much would you like to deposit?")
-        new_balance = self.account_bal + float(add)
-        print(new_balance)
-        ledger = open("account_ledger.txt", "a")
-        ledger.write(f"\n+{add}")
-        new_balance_f = str(new_balance)
-        bal = (f"\n {new_balance_f:.2f} balance")
-        ledger.write(bal.format(bal))
-        ledger.close()
+
+        with open("account_ledger.txt", "r") as f:
+            current_bal = f.readlines()[-1]
+
+        change = f"\n+{add}"
+        new_balance = float(current_bal) + float(add)
+
+        self.write_changes(change, new_balance)
 
     def view(self):
         with open("account_ledger.txt", "r") as account:
@@ -68,5 +64,7 @@ while True:
         atm.deposit()
     elif atm_action == 'v':
         atm.view()
+    elif atm_action == 'q':
+        break
     else:
-        print("Please select the letter next to the action you wish to take \nW: Withdraw \nD: Deposit \nV: View account")
+        print("Please select the letter next to the action you wish to take \nW: Withdraw \nD: Deposit \nV: View account \nQ: Quit")
--- account_ledger.py
+++
@@ -1,2 +0,0 @@
-
-7777.77 starting balance Mon Mar 28 22:27:06 2022
